155ca4239e3a9b758d32fcb1e962ec6021c71b5c
Louis Liu <louis.wenchao.liu@gmail.com>
2014-09-16 15:00:02 +0800

    add console and command.

---
+++ app/scripts/command.js
@@ -0,0 +1,8 @@
+exports = module.exports = new Command();
+
+exports.Command = Command;
+
+exports.Option = Option;
+
+function Command(){}
+function Option(){}
---
+++ app/scripts/console.js
@@ -0,0 +1,34 @@
+// exports = module.exports = new Runner();
+
+// exports.Runner = Runner;
+// Runner = function(){}();
+function Runner(){
+  this.container = document.querySelector('main');
+};
+
+runner = new Runner();
+
+Runner.prototype.run = function(form) {
+  if (form.command.value) {
+    this.render(form.command.value, form.command.value);
+    form.command.value = '';
+    location.href='#console';
+  }
+}
+
+Runner.prototype.render = function(cmd, result) {
+  var now = new Date();
+  var html = [
+    '<div class="container theme--user-input"><h4>',
+    cmd,
+    '<span class="themed">',
+    ' [',
+    now.toLocaleTimeString(),
+    ']',
+    '</span></h4><div>',
+    result,
+    '</div></div><hr />'
+  ];
+
+  this.container.insertAdjacentHTML('beforeend', html.join(''));
+}
