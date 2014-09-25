51f650d613d5a5090256f33b681ed4eb5db92b31
Louis Liu <louis.wenchao.liu@gmail.com>
2014-09-25 19:19:48 +0800

    allow user to change repository path.

--- app/scripts/github.js
+++ app/scripts/github.js
@@ -1,30 +1,29 @@
-var repo = 'liuwenchao/aha-table';
-var root = 'https://api.github.com/repos/'+repo+'/contents/';
 var tree = {
-  name: ko.observable(repo),
+  repo: ko.observable('liuwenchao/aha-table'),
   viewing: ko.observable(),
   children: ko.observableArray()
 };
 
-function loadChildren(url, parent) {
+function loadChildren(parent, url) {
+  var url_root = 'https://api.github.com/repos/' + tree.repo()+'/contents/';
   // parent.children = parent.children || ko.observableArray();
-  $.getJSON(url, function(children){
+  $.getJSON(url ? url : url_root, function(children){
     for (var i = children.length - 1; i >= 0; i--) {
       var child = children[i];
       var file = {
         name: child.name, 
-        link: root + child.path,
+        link: url_root + child.path,
         type: child.type,
         path: child.path,
-        postfix: child.type === 'file' ? child.name.substr(child.name.lastIndexOf('.') - child.name.length + 1) : '',
         open: ko.observable(false),
         click: child.type === 'file' ? loadContent : toggleChildren,
         content: child.type === 'file' ? ko.observable() : false,
+        postfix: child.type === 'file' ? child.name.substr(child.name.lastIndexOf('.') - child.name.length + 1) : '',
         children: ko.observableArray()
       };
       switch(child.type) {
         case 'dir':
-          loadChildren(file.link, file, false);
+          loadChildren(file, file.link);
           break;
         case 'file':
         case 'symlink':
@@ -34,6 +33,9 @@ function loadChildren(url, parent) {
       }
       parent.children.push(file);
     };
+  })
+  .fail(function(){
+    alert('Does not exist, check the path again.');
   });
 }
 
@@ -53,8 +55,22 @@ function toggleChildren() {
   this.open(!this.open());
 }
 
+function loadRepo(newRepo) {
+  repo = newRepo;
+  tree.viewing(undefined);
+  tree.children.removeAll();
+  loadChildren(tree);
+}
+
+function search(event) {
+  var key = event.key || event.which
+  if (key == 191) {
+    //fetch repos of this user.
+  }
+}
+
 hljs.initHighlightingOnLoad();
 $(document).ready(function(){
-  loadChildren(root, tree);
+  loadChildren(tree);
   ko.applyBindings(tree, document.body);
 });
