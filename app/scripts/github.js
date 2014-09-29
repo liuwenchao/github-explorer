a9a60d5e184831a6eabf9d2aa927c435c4644261
Louis Liu <louis.wenchao.liu@gmail.com>
2014-09-29 18:50:48 +0800

    add OAuth2 integration.

--- app/scripts/github.js
+++ app/scripts/github.js
@@ -9,7 +9,14 @@ var tree = {
     tree.children.removeAll();
     loadChildren(tree);
     return false;
-  }
+  },
+  rate: {
+    limit: ko.observable(),
+    remaining: ko.observable(),
+    reset: ko.observable()
+  },
+  login: OAuth.login,
+  isLoggedIn: ko.observable(false)
 };
 var ko = window.ko,
     $ = window.$,
@@ -20,7 +27,9 @@ var ko = window.ko,
 function loadChildren(parent, url) {
   var urlRoot = 'https://api.github.com/repos/' + tree.repo()+'/contents/';
   // parent.children = parent.children || ko.observableArray();
-  $.getJSON(url ? url : urlRoot, function(children){
+  OAuth.request(url ? url : urlRoot)
+  .success(function(children){
+    monitorRate();
     for (var i = children.length - 1; i >= 0; i--) {
       var child = children[i];
       var file = {
@@ -49,15 +58,18 @@ function loadChildren(parent, url) {
     }
   })
   .fail(function(){
+    monitorRate();
+    tree.isLoggedIn(false);
     window.alert('Path error or limit is reached for API calling.');
   });
 }
 
 var loadContent = function() {
   if (this.content() === undefined) {
-    $.getJSON(this.link, function(fileData) {
+    OAuth.request(this.link).success(function(fileData) {
       this.content(atob(fileData.content));
     }.bind(this));
+    monitorRate();
   }
   tree.viewing(this);
   this.open(true);
@@ -68,7 +80,32 @@ var toggleChildren = function() {
   this.open(!this.open());
 };
 
+var monitorRate = function() {
+  OAuth.getRate().success(function(data){
+    tree.rate.limit(data.rate.limit);
+    tree.rate.remaining(data.rate.remaining);
+    var reset = new Date(data.rate.reset*1000);
+    tree.rate.reset(reset.toLocaleTimeString());
+    tree.isLoggedIn(OAuth.config.isLoggedIn);
+    if (data.rate.remaining === 0) {
+      tree.isLoggedIn(false);
+    }
+  });
+};
+
 $(document).ready(function(){
-  loadChildren(tree);
+  if (window.location.search) {
+    var params = window.location.search.substr(1).split('&');
+    OAuth.postLogin({
+      code: params[0].split('=')[1], 
+      state: params[1].split('=')[1]
+    }).always(function(){
+      monitorRate();
+      loadChildren(tree);
+    });
+  } else {
+    monitorRate();
+    loadChildren(tree);
+  }
   ko.applyBindings(tree, document.body);
 });
