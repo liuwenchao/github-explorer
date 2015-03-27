936975b0b5843c23d31cef263c745f81077e86b4
Louis Liu <louis.wenchao.liu@gmail.com>
2015-03-27 14:06:22 +0800

    update psi site url.

--- gulpfile.js
+++ gulpfile.js
@@ -194,7 +194,7 @@ gulp.task('pagespeed', pagespeed.bind(null, {
   // free (no API key) tier. You can use a Google
   // Developer API key if you have one. See
   // http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
-  url: 'https://example.com',
+  url: 'https://liuwenchao.github.com/github-explorer/github.html',
   strategy: 'mobile'
 }));
 
